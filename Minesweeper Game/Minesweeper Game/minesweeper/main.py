5d645931ed2514afe0861c99d3e1e4a8f27b7c96
Maria Rosenova <93831078+MariaRosenova@users.noreply.github.com>
2024-06-13 10:44:04 +0300

    add tkinter

---
+++ Minesweeper Game/Minesweeper Game/minesweeper/main.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+root = Tk()
+#Override the settings of the window
+root.configure(bg="black")
+root.geometry('1440x720')
+root.title('Minesweeper Game')
+root.resizable(False, False)
+
+top_frame = Frame(
+    root,
+    bg='red', #Change later to black
+    width=1440,
+    height=180
+)
+top_frame.place(x=0, y=0)
+
+#left side bar
+left_frame = Frame(
+    bg="blue", #Change later to black
+    width=360,
+    height=540
+)
+left_frame.place(x=0, y=180)
+
+#Run the window
+root.mainloop()
